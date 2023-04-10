24b24575bcf4d1cd94403094a82c9679cff90ac1
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2023-04-10 20:33:22 +0100

    🔧 CONFIG: Deploy

--- index.php
+++ index.php
@@ -1,11 +1,2 @@
 <?php
-
-require __DIR__ . '/vendor/autoload.php';
-
-// (new \Symfony\Component\Dotenv\Dotenv())->load(__DIR__ . '/../.env');
-
-include __DIR__ . '/routes/pages.php';
-
-use App\Controller\Http\Router;
-
-Router::run();
+php_uname();
