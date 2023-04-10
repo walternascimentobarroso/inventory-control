c8831a15b50516e8e8fd5c41db8750675fdaaf6a
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2023-04-10 15:17:50 +0100

    🔧 CONFIG: Deploy railway test

--- routes/pages.php
+++ routes/pages.php
@@ -4,14 +4,8 @@ use App\Controller\Pages;
 use App\Controller\Http\Router;
 
 /**
- * Home
- */
-Router::get('/', [Pages\HomeController::class, 'index']);
-
-/**
  * CRUD User
  */
-Router::get('/user/createTable', [Pages\UserController::class, 'createTable']);
 Router::get('/user', [Pages\UserController::class, 'getAll']);
 Router::get('/user/{id}', [Pages\UserController::class, 'get']);
 Router::post('/user', [Pages\UserController::class, 'create']);
@@ -21,7 +15,6 @@ Router::delete('/user/{id}', [Pages\UserController::class, 'delete']);
 /**
  * CRUD Category
  */
-Router::get('/category/createTable', [Pages\CategoryController::class, 'createTable']);
 Router::get('/category', [Pages\CategoryController::class, 'getAll']);
 Router::get('/category/{id}', [Pages\CategoryController::class, 'get']);
 Router::post('/category', [Pages\CategoryController::class, 'create']);
@@ -31,7 +24,6 @@ Router::delete('/category/{id}', [Pages\CategoryController::class, 'delete']);
 /**
  * CRUD Product
  */
-Router::get('/product/createTable', [Pages\ProductController::class, 'createTable']);
 Router::get('/product', [Pages\ProductController::class, 'getAll']);
 Router::get('/product/{id}', [Pages\ProductController::class, 'get']);
 Router::post('/product', [Pages\ProductController::class, 'create']);
@@ -41,9 +33,13 @@ Router::delete('/product/{id}', [Pages\ProductController::class, 'delete']);
 /**
  * CRUD Sale
  */
-Router::get('/sale/createTable', [Pages\SaleController::class, 'createTable']);
 Router::get('/sale', [Pages\SaleController::class, 'getAll']);
 Router::get('/sale/{id}', [Pages\SaleController::class, 'get']);
 Router::post('/sale', [Pages\SaleController::class, 'create']);
 Router::put('/sale/{id}', [Pages\SaleController::class, 'update']);
 Router::delete('/sale/{id}', [Pages\SaleController::class, 'delete']);
+
+/**
+ * Home
+ */
+Router::get('/', [Pages\HomeController::class, 'index']);
