36bd98a7dc31b722de8cbfc6700a100021d2957e
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2023-04-10 14:03:55 +0100

    🔧 CONFIG: Deploy railway test

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require __DIR__ . '/vendor/autoload.php';
 print_r($_ENV);
+die;
 // (new \Symfony\Component\Dotenv\Dotenv())->load(__DIR__ . '/../.env');
 
 include __DIR__ . '/routes/pages.php';
