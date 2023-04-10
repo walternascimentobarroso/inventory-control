5b0555dbdbd213ff7006285d06c3a60d05dea2d7
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2023-04-10 23:33:09 +0100

    🚑 HOTFIX: Fix CORS

--- public/index.php
+++ public/index.php
@@ -8,4 +8,7 @@ include __DIR__ . '/../routes/pages.php';
 
 use App\Controller\Http\Router;
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: *');
+header("Access-Control-Allow-Headers: *");
 Router::run();
