093b64466b1806bc6c318e98b69228cd5a750dfc
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2023-04-13 12:44:53 +0100

    🔧 CONFIG: Create database

--- db/script.sql
+++ db/script.sql
@@ -1,29 +1,25 @@
 CREATE TABLE public.categories (
-	id serial4 NOT NULL,
-	description varchar NULL,
-	CONSTRAINT categories_pkey PRIMARY KEY (id)
+	id serial PRIMARY KEY,
+	description varchar NULL
 );
 
 CREATE TABLE public.products (
-	id serial4 NOT NULL,
+	id serial PRIMARY KEY,
 	description varchar NULL,
 	category int4 NULL,
 	barcode varchar NULL,
 	value varchar NULL,
-	tax varchar NULL,
-	CONSTRAINT products_pkey PRIMARY KEY (id)
+	tax varchar NULL
 );
 
 CREATE TABLE public.sales (
-	id serial4 NOT NULL,
+	id serial PRIMARY KEY,
 	items varchar(255) NULL,
-	total int4 NULL,
-	CONSTRAINT sales_pkey PRIMARY KEY (id)
+	total int4 NULL
 );
 
 CREATE TABLE public.users (
-	id serial4 NOT NULL,
+	id serial PRIMARY KEY,
 	name varchar(255) NULL,
-	email varchar(255) NULL,
-	CONSTRAINT users_pkey PRIMARY KEY (id)
+	email varchar(255) NULL
 );
--- index.php
+++ index.php
@@ -2,10 +2,57 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-// (new \Symfony\Component\Dotenv\Dotenv())->load(__DIR__ . '/../.env');
+use App\Model\DAO\Product;
+use App\Controller\Http\Db\Connection;
 
-include __DIR__ . '/routes/pages.php';
+(new \Symfony\Component\Dotenv\Dotenv())->load(__DIR__ . '/.env');
 
-use App\Controller\Http\Router;
+function createDATABASE()
+{
+    $connection = (new Connection())->connect();
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $query = "CREATE DATABASE postgresss";
+    $connection->exec($query);
+}
 
-Router::run();
+function createTables()
+{
+    $commands = <<<TABLE
+        CREATE TABLE public.categories (
+            id serial PRIMARY KEY,
+            description varchar NULL
+        );
+
+        CREATE TABLE public.products (
+            id serial PRIMARY KEY,
+            description varchar NULL,
+            category int4 NULL,
+            barcode varchar NULL,
+            value varchar NULL,
+            tax varchar NULL
+        );
+
+        CREATE TABLE public.sales (
+            id serial PRIMARY KEY,
+            items varchar(255) NULL,
+            total int4 NULL
+        );
+
+        CREATE TABLE public.users (
+            id serial PRIMARY KEY,
+            name varchar(255) NULL,
+            email varchar(255) NULL
+        );
+TABLE;
+
+    try {
+        $connection = (new Connection())->connect();
+        $connection->exec($commands);
+        return "Success!\n";
+    } catch (\PDOException $e) {
+        exit($e->getMessage());
+    }
+}
+
+createDATABASE();
+createTables();
